ab68b90dbc452d84ccdae57af7436021c36e6e15
MrB4N4N4 <2016rudgns@naver.com>
2021-09-07 02:04:55 -0400

    simple web scanner

--- src/process.py
+++ src/process.py
@@ -81,51 +81,7 @@ def port_to_table(info):
     table.add_column("Protocol")
     table.add_column("Port Number")
     table.add_column("Name")
-    table.add
-### **[Modules]**
-
-#### root identification
-
-#### argparse
-
-#### scan
-<ul>network</ul>
-<ul>ports</ul>
-
-#### print
-<ul>logo & usage</ul>
-<ul>loading icon(ft.thread)</ul>
-<ul>vulns</ul>
-<ul>exploitDB</ul>
-<ul>available tools & usage</ul>
-
-indicators
-
-... : loading
-
-[+] : success
-
-[-] : error
-
-[!] : warning
-
-[i] : info
-
----
-
-### Work Save Point
-
-#### Implement
-
-- check.is_root, get_localhost, argparser, Nmap
-
-#### To Do
-
-print Nmap results, add rich loading icon.
-
-study awesome library `rich` for pretty console. then,
-
----_column("Product")
+    table.add_column("Product")
     table.add_column("Version")
 
     os_info, service_info = info[0], info[1]
